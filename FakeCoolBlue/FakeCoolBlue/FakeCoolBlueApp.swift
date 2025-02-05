3078e8864128ded4c6f2555295a3a182c091857a
Clinton Davelaar <c.davelaar@brightfish.nl>
2025-02-05 15:01:15 +0100

    Setup project

---
+++ FakeCoolBlue/FakeCoolBlue/FakeCoolBlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeCoolBlueApp.swift
+//  FakeCoolBlue
+//
+//  Created by Clinton on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeCoolBlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
