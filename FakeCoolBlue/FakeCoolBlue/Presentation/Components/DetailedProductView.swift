35ebff03f106f4921a174c5fdb851645cc2df147
Clinton Davelaar <c.davelaar@brightfish.nl>
2025-02-10 14:14:54 +0100

    Minor clean up

--- FakeCoolBlue/FakeCoolBlue/Presentation/Components/DetailedProductView.swift
+++ FakeCoolBlue/FakeCoolBlue/Presentation/Components/DetailedProductView.swift
@@ -15,45 +15,14 @@ struct DetailedProductView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Image(systemName: "xmark.circle.fill")
-                .foregroundStyle(.gray)
-                .background(Color.white)
-                .clipShape(Circle())
-                .onTapGesture {
-                    onClose()
-                }
+            closeButton()
             ProductImageView(animation: animation, id: product.productId, urlString: product.productImage, width: .infinity, height: 240)
-            Text(product.productName)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.headline)
-                .bold()
-                .matchedGeometryEffect(id: "title_\(product.productId)", in: animation)
-            if product.nextDayDelivery {
-                Text("Order before 17:00 and get next day delivery")
-            }
+            titleRow()
+            deliveryRow()
             UniqueSellingPointView(animation: animation, product: product)
-            HStack {
-                Text("Price:")
-                Spacer()
-                Text("\(product.salesPriceIncVat.formattedAsEuro())")
-            }
+            priceRow()
             Spacer()
-            Button {
-
-            } label: {
-                HStack {
-                    Text("Add to Cart")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                    Image(systemName:"cart.fill")
-                        .foregroundColor(.white)
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .center)
-                .background(Color.blue.opacity(0.2))
-                .cornerRadius(8)
-            }
-            .padding(.bottom)
+            addToCartButton()
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
@@ -63,6 +32,62 @@ struct DetailedProductView: View {
                 .matchedGeometryEffect(id: "background_\(product.productId)", in: animation)
         )
         .ignoresSafeArea(edges: .bottom)
+    }
+
+    @ViewBuilder
+    func closeButton() -> some View {
+        Image(systemName: "xmark.circle.fill")
+            .foregroundStyle(.gray)
+            .background(Color.white)
+            .clipShape(Circle())
+            .onTapGesture {
+                onClose()
+            }
+    }
+
+    @ViewBuilder
+    func titleRow() -> some View {
+        Text(product.productName)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.headline)
+            .bold()
+            .matchedGeometryEffect(id: "title_\(product.productId)", in: animation)
+    }
+
+    @ViewBuilder
+    func deliveryRow() -> some View {
+        if product.nextDayDelivery {
+            Text("Order before 17:00 and get next day delivery")
+        }
+    }
+
+    @ViewBuilder
+    func priceRow() -> some View {
+        HStack {
+            Text("Price:")
+            Spacer()
+            Text("\(product.salesPriceIncVat.formattedAsEuro())")
+        }
+    }
+
+    @ViewBuilder
+    func addToCartButton() -> some View {
+        Button {
+
+        } label: {
+            HStack {
+                Text("Add to Cart")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Image(systemName:"cart.fill")
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .center)
+            .background(Color.blue.opacity(0.2))
+            .cornerRadius(8)
+        }
+        .padding(.bottom)
     }
 }
 
